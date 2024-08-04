ded4c361cb04f5f86a08401015f79095275336f7
Tamara <Tklmnv@list.ru>
2024-08-04 16:26:34 +0500

    решение

--- ty.py
+++ ty.py
@@ -1,1 +1,16 @@
-try
+import pandas as pd
+import random
+
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI': lst})
+
+# Создаем столбцы для каждого уникального значения в 'whoAmI'
+for value in data['whoAmI'].unique():
+    data[value] = (data['whoAmI'] == value).astype(int)
+
+# Удаляем исходный столбец 'whoAmI'
+data = data.drop('whoAmI', axis=1)
+
+print(data.head())
